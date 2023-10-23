cb315c9f343d2b127b6f43f4476bc285c8b34353
joytzli <joytzli@tencent.com>
2023-10-23 10:40:15 +0800

    sql

---
+++ packages/init-database/src/sql/insert_material_info.sql
@@ -0,0 +1,20 @@
+INSERT INTO material_info
+  (uuid, name, current_version, status, info, extend )
+VALUES 
+  (
+    '11110000-aaaa-bbbb-cccc-ddddeeee0001',
+    '@joy-material/promoted-products',
+    '1.2.3',
+    1,
+    '{}',
+    '{}'
+  ),
+   (
+    '11110000-aaaa-bbbb-cccc-ddddeeee0002',
+    '@joy-material/promoted-banner',
+    '4.5.6',
+    1,
+    '{}',
+    '{}'
+  )
+;
